5023a3c6625f612e4b008aa18bb6d65b02eaf1be
ericonyango <erconyango@gmail.com>
2025-03-13 15:11:46 +0300

    all project

--- public/index.php
+++ public/index.php
@@ -1,11 +1,7 @@
 <?php
 
-echo "<pre>";
-print_r($_SERVER);
-echo "</pre>";
-
-
-
+include __DIR__ . "/../src/App/functions.php";
 $app = include __DIR__ . '/../src/App/bootstrap.php';
 
 $app->run();
+
--- src/Framework/App.php
+++ src/Framework/App.php
@@ -5,8 +5,27 @@ declare(strict_types=1);
 namespace Framework;
 class App
 {
+    //the property is the name of $router class in lower letters.
+    //return type is the Router class.
+    private Router $router;
+
+    //set this property during initiation
+
+    public function __construct()
+    {
+        $this->router= new Router();
+    }
+
     public function run()
     {
-        echo "halloooooooo!";
+        $path=parse_url($_SERVER['REQUEST_URI'],PHP_URL_PATH);
+        $method=$_SERVER['REQUEST_METHOD'];
+
+        $this->router->dispatch($path,$method);
+    }
+
+    public function get(string $path,array $controller)
+    {
+        $this->router->add('GET',$path,$controller);
     }
 }
